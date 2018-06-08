3c537d51ae52c27aa550c83e2cf16a6216483c23
Tutch <lucasmapurunga@gmail.com>
2018-06-08 09:08:18 -0300

    Servidor para servir a pagina da interface

---
+++ app/report-server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+class ReportServer {
+    constructor(port, options={}){
+        this.reports = {};
+        
+        this.serveInterface(options);
+        
+        app.listen(port, () => {
+            console.log(`im alive at port ${port}`);
+        })
+
+        app.get('/report/rule-violations', (req, res) => {
+            res.send(this.reports['rule-violations']);
+        });
+    }
+
+    serveInterface(options) {
+        // if(options['interface-folder'] == undefined) {
+        //     app.use(express.static(options['interface-folder']));
+        // }else {
+            app.use(express.static('interface'));
+        // }
+    }
+
+    setRuleViolations(data) {
+        this.reports['rule-violations'] = data;
+    }
+}
+
+
+
+module.exports = ReportServer;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var loadReport = require('./app/report-loader');
+var ReportServer = require('./app/report-server');
 var file = './reports/rule-violation-2018-06-06-16-35-49.csv';
+
+var server = new ReportServer(6060);
 
 // Carrega os dados do relatório desejado.
 loadReport(file).then((parsedData) =>{
-    let ruleIds = Object.keys(parsedData);
-    console.log(parsedData);
-    console.log(ruleIds);
+    server.setRuleViolations(parsedData);
 }).catch(err => {
     console.log(err);
 });
