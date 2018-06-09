d060a415948a12bc2ac4381e98f22840e957337f
lucas.mapurunga <lucasmapurunga@gmail.com>
2018-06-09 20:26:09 -0300

    Lista sendo passada a partir da regra escolhida

--- interface/src/main.js
+++ interface/src/main.js
@@ -1,31 +1,59 @@
 // Selecionar regra
 Vue.component('pick-rule', {
-    template: '<div>select</div>'
+    props: ['items'],
+    data() {
+        return {
+            choosenRule:''
+        }
+    },
+    template: `
+        <div>
+            <select v-model="choosenRule" @change="pickedRule">
+                <option disabled value="">Choose violation rule</option>
+                <option v-for="item in items">{{item}}</option>
+            </select>
+        </div>
+    `,
+    methods: {
+        pickedRule: function() {
+            this.$emit('violation-rule-picked', this.choosenRule);
+        }
+    }
 })
 
 // Gráfico
 Vue.component('rule-graph', {
-    template: '<div>graph</div>'
+    props: ['violations'],
+    template: `
+        <div>{{violations}}</div>
+    `
 })
 
 // App
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        violations_from_rule: [],
+        rule_violations: [],
+        violation_headers: [],
     },
     created() {
-        this.getRuleViolations();
+       this.getRuleViolations();
     },
     methods: {
         getRuleViolations: function() {
             this.$http.get('http://localhost:6060/reports/rule-violations')
             .then(response => {
-                console.log(JSON.parse(response.bodyText));     
+                this.rule_violations = JSON.parse(response.bodyText); 
+                this.violation_headers = Object.keys(JSON.parse(response.bodyText));    
             })
             .catch(error => {
                 console.log(error.statusText)
             });
+        },
+        ruleFromPickRule: function(rulename) {
+            this.choosen_rule = rulename;
+            this.violations_from_rule = this.rule_violations[rulename];
         }
     }
 })
