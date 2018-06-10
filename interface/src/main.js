3d7135108cb4a722c60a0c50003d386b117dedb4
lucas.mapurunga <lucasmapurunga@gmail.com>
2018-06-09 21:15:11 -0300

    Adicionando vis e mostrando um grafico base

--- interface/src/main.js
+++ interface/src/main.js
@@ -25,8 +25,48 @@ Vue.component('pick-rule', {
 Vue.component('rule-graph', {
     props: ['violations'],
     template: `
-        <div>{{violations}}</div>
-    `
+        <div id="rule-graph"></div>
+    `,
+    watch: {
+        violations: function() {
+            var nodes = new vis.DataSet([
+                {id: 1, label: 'Node 1'},
+                {id: 2, label: 'Node 2'},
+                {id: 3, label: 'Node 3'},
+                {id: 4, label: 'Node 4'},
+                {id: 5, label: 'Node 5'}
+            ]);
+        
+            // create an array with edges
+            var edges = new vis.DataSet([
+                {from: 1, to: 3},
+                {from: 1, to: 2},
+                {from: 2, to: 4},
+                {from: 2, to: 5}
+            ]);
+        
+            // create a network
+            var container = document.getElementById('rule-graph');
+        
+            // provide the data in the vis format
+            var data = {
+                nodes: nodes,
+                edges: edges
+            };
+
+            var options = {
+                autoResize: true,
+                height:'100%',
+                width:'100%',
+                physics: {
+                   enabled: false 
+                }
+            };
+        
+            // initialize your network!
+            var network = new vis.Network(container, data, options);
+        }
+    }
 })
 
 // App
